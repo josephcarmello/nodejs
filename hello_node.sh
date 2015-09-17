b01c0c6a74b382f4270b0859bf6136f5719bfebb
Joey Carmello <joey.carmello@1stdibs.com>
2015-09-17 18:06:22 -0400

    Changed from a node init start to a forever script

--- hello_node.sh
+++ hello_node.sh
@@ -4,8 +4,7 @@ echo "Starting a default node app on port 8194"
 
 #Start the app and fork it to the background
 
-su vagrant -c 'node /vagrant/hello_node.js < /dev/null &'
-
-#Let 'em know its up
+forever start /vagrant/hello_node.js
+# Let 'em know its up
 
 echo "App is up and running on http://127.0.0.1:8194"
