9d85a2dc4da68800b872d766a5162ed3bae49b5d
Joey Carmello <joey.carmello@1stdibs.com>
2015-09-17 17:43:42 -0400

    Turns out its my first day in linux...made the file a script and added some output so it wasnt just a bullshit text file.

--- hello_node.sh
+++ hello_node.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#Let the user knowa node app is starting
+echo "Starting a default node app on port 8194"
+
+#Start the app and fork it to the background
+
 sudo node hello_node.js &
+
+#Let 'em know its up
+
+echo "App is up and running on http://127.0.0.1:8194"
